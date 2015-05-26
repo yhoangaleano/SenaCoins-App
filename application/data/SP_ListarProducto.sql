75493a8e61d35b52bb4be8d0693adcc33b33ee7b
sergiolondon2410 <sergiolondon2410@gmail.com>
2015-05-26 10:56:08 -0500

    Commit sergio v2

--- application/data/SP_ListarProducto.sql
+++ application/data/SP_ListarProducto.sql
@@ -2,4 +2,4 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_ListarProducto`()
     NO SQL
 SELECT producto.idProducto, producto.nombre_producto, producto.descripcion, producto.url_guia, producto.equipo_idEquipo ,equipo.nombre_equipo,(SELECT SUM(cantidad) FROM transaccion WHERE producto_idProducto = producto.idProducto) AS inversion, (SELECT url_imagen FROM imagen WHERE producto_idProducto = producto.idProducto AND estado = 1 LIMIT 1) AS imagen
     FROM producto
-    INNER JOIN equipo ON producto.equipo_idEquipo = equipo.idEquipo
+    INNER JOIN equipo ON producto.equipo_idEquipo = equipo.idEquipo ORDER BY inversion DESC
