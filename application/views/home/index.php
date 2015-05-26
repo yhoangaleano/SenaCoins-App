3dab90c38215759d0f5dd8b5e8b296050ab9ccf2
--replace-all <tonho0593@hotmail.com>
2015-05-26 09:01:52 -0500

    Init

---
+++ application/views/home/index.php
@@ -0,0 +1,243 @@
+<!-- BEGIN BLOCKQUOTE BLOCK -->
+<div class="row quote-v1 margin-bottom-30">
+    <div class="col-md-9">
+        <span>Artefactos realiazados en la III Jornada Pedagógica</span>
+    </div>
+    <div class="col-md-3 pull-right">
+        <a href="javascript:;" class="btn-block text-center btn-transparent"><i class="fa fa-search margin-right-10"></i>Ver artefactos</a>
+    </div>
+</div>
+<!-- END BLOCKQUOTE BLOCK -->
+<!-- BEGIN RECENT WORKS -->
+<div class="row recent-work margin-bottom-40">
+    <div class="col-md-3">
+        <h2><a href="http://keenthemes.com/preview/metronic/theme/templates/frontend/portfolio.html">Ver artefactos recientes</a></h2>
+        <p class="text-justify">Esta sección muestra los diferentes artefactos creados en la <b> III Jornada Pedagógica </b> realizada en el CESGE. Todos los equipos usaron su creatividad para realizar el mejor artefacto. Para invertir en alguno, siga las instrucciones que se muestran en el panel inferior.</p>
+    </div>
+    <div class="col-md-9">
+        <div class="owl-carousel owl-carousel3">
+            <div class="recent-work-item">
+                <em>
+                    <img src="img/works/img1.jpg" alt="Amazing Project" class="img-responsive">
+                    <a href="portfolio-item.html"><i class="fa fa-link"></i></a>
+                    <a href="img/works/img1.jpg" class="fancybox-button" title="Project Name #1" data-rel="fancybox-button"><i class="fa fa-search"></i></a>
+                </em>
+                <a class="recent-work-description" href="javascript:;">
+                    <strong>Amazing Project</strong>
+                    <b>Agenda corp.</b>
+                </a>
+            </div>
+            <div class="recent-work-item">
+                <em>
+                    <img src="img/works/img2.jpg" alt="Amazing Project" class="img-responsive">
+                    <a href="portfolio-item.html"><i class="fa fa-link"></i></a>
+                    <a href="img/works/img2.jpg" class="fancybox-button" title="Project Name #2" data-rel="fancybox-button"><i class="fa fa-search"></i></a>
+                </em>
+                <a class="recent-work-description" href="javascript:;">
+                    <strong>Amazing Project</strong>
+                    <b>Agenda corp.</b>
+                </a>
+            </div>
+            <div class="recent-work-item">
+                <em>
+                    <img src="img/works/img3.jpg" alt="Amazing Project" class="img-responsive">
+                    <a href="portfolio-item.html"><i class="fa fa-link"></i></a>
+                    <a href="img/works/img3.jpg" class="fancybox-button" title="Project Name #3" data-rel="fancybox-button"><i class="fa fa-search"></i></a>
+                </em>
+                <a class="recent-work-description" href="javascript:;">
+                    <strong>Amazing Project</strong>
+                    <b>Agenda corp.</b>
+                </a>
+            </div>
+            <div class="recent-work-item">
+                <em>
+                    <img src="img/works/img4.jpg" alt="Amazing Project" class="img-responsive">
+                    <a href="portfolio-item.html"><i class="fa fa-link"></i></a>
+                    <a href="img/works/img4.jpg" class="fancybox-button" title="Project Name #4" data-rel="fancybox-button"><i class="fa fa-search"></i></a>
+                </em>
+                <a class="recent-work-description" href="javascript:;">
+                    <strong>Amazing Project</strong>
+                    <b>Agenda corp.</b>
+                </a>
+            </div>
+            <div class="recent-work-item">
+                <em>
+                    <img src="img/works/img5.jpg" alt="Amazing Project" class="img-responsive">
+                    <a href="portfolio-item.html"><i class="fa fa-link"></i></a>
+                    <a href="img/works/img5.jpg" class="fancybox-button" title="Project Name #5" data-rel="fancybox-button"><i class="fa fa-search"></i></a>
+                </em>
+                <a class="recent-work-description" href="javascript:;">
+                    <strong>Amazing Project</strong>
+                    <b>Agenda corp.</b>
+                </a>
+            </div>
+            <div class="recent-work-item">
+                <em>
+                    <img src="img/works/img6.jpg" alt="Amazing Project" class="img-responsive">
+                    <a href="portfolio-item.html"><i class="fa fa-link"></i></a>
+                    <a href="img/works/img6.jpg" class="fancybox-button" title="Project Name #6" data-rel="fancybox-button"><i class="fa fa-search"></i></a>
+                </em>
+                <a class="recent-work-description" href="javascript:;">
+                    <strong>Amazing Project</strong>
+                    <b>Agenda corp.</b>
+                </a>
+            </div>
+            <div class="recent-work-item">
+                <em>
+                    <img src="img/works/img3.jpg" alt="Amazing Project" class="img-responsive">
+                    <a href="portfolio-item.html"><i class="fa fa-link"></i></a>
+                    <a href="img/works/img3.jpg" class="fancybox-button" title="Project Name #3" data-rel="fancybox-button"><i class="fa fa-search"></i></a>
+                </em>
+                <a class="recent-work-description" href="javascript:;">
+                    <strong>Amazing Project</strong>
+                    <b>Agenda corp.</b>
+                </a>
+            </div>
+            <div class="recent-work-item">
+                <em>
+                    <img src="img/works/img4.jpg" alt="Amazing Project" class="img-responsive">
+                    <a href="portfolio-item.html"><i class="fa fa-link"></i></a>
+                    <a href="img/works/img4.jpg" class="fancybox-button" title="Project Name #4" data-rel="fancybox-button"><i class="fa fa-search"></i></a>
+                </em>
+                <a class="recent-work-description" href="javascript:;">
+                    <strong>Amazing Project</strong>
+                    <b>Agenda corp.</b>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- END RECENT WORKS -->
+<!-- BEGIN BLOCKQUOTE BLOCK -->
+<div class="row quote-v1 margin-bottom-30">
+    <div class="col-md-9">
+        <span>Pasos para invertir en artefactos</span>
+    </div>
+</div>
+<!-- END BLOCKQUOTE BLOCK -->
+<!-- BEGIN STEPS -->
+<div class="row margin-bottom-40 front-steps-wrapper front-steps-count-3">
+    <div class="col-md-4 col-sm-4 front-step-col">
+        <div class="front-step front-step1">
+            <h2>Iniciar sesión</h2>
+            <p>Ingresa con el usuario y la contraseña asignada a cada equipo.</p>
+        </div>
+    </div>
+    <div class="col-md-4 col-sm-4 front-step-col">
+        <div class="front-step front-step2">
+            <h2>Selecciona el artefacto</h2>
+            <p>Con tu equipo decide en que artefacto vas a invertir. Recuerda visualizar las imágenes y tomar una buena decisión.</p>
+        </div>
+    </div>
+    <div class="col-md-4 col-sm-4 front-step-col">
+        <div class="front-step front-step3">
+            <h2>Invierte</h2>
+            <p>Realiza la inversión al artefacto que decidas, recuerda que cada equipo tiene un monto de $100 SENA Coins.</p>
+        </div>
+    </div>
+</div>
+<!-- END STEPS -->
+<!-- BEGIN TABS AND TESTIMONIALS -->
+<div class="row mix-block margin-bottom-40">
+    <!-- TESTIMONIALS -->
+    <div class="col-md-6 testimonials-v1">
+
+        <!-- BEGIN BLOCKQUOTE BLOCK -->
+        <div class="row quote-v1 margin-bottom-30">
+            <div class="col-md-12">
+                <span>Comentarios de la III Jornada Pedagógica</span>
+            </div>
+        </div>
+        <!-- END BLOCKQUOTE BLOCK -->
+        <div id="myCarousel" class="carousel slide">
+            <!-- Carousel items -->
+            <div class="carousel-inner">
+                <div class="active item">
+                    <blockquote><p>Denim you probably haven't heard of. Lorem ipsum dolor met consectetur adipisicing sit amet, consectetur adipisicing elit, of them jean shorts sed magna aliqua. Lorem ipsum dolor met.</p></blockquote>
+                    <div class="carousel-info">
+                        <img class="pull-left" src="img/people/img1-small.jpg" alt="">
+                        <div class="pull-left">
+                            <span class="testimonials-name">Lina Mars</span>
+                            <span class="testimonials-post">Commercial Director</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <blockquote><p>Raw denim you Mustache cliche tempor, williamsburg carles vegan helvetica probably haven't heard of them jean shorts austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica.</p></blockquote>
+                    <div class="carousel-info">
+                        <img class="pull-left" src="img/people/img5-small.jpg" alt="">
+                        <div class="pull-left">
+                            <span class="testimonials-name">Kate Ford</span>
+                            <span class="testimonials-post">Commercial Director</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <blockquote><p>Reprehenderit butcher stache cliche tempor, williamsburg carles vegan helvetica.retro keffiyeh dreamcatcher synth. Cosby sweater eu banh mi, qui irure terry richardson ex squid Aliquip placeat salvia cillum iphone.</p></blockquote>
+                    <div class="carousel-info">
+                        <img class="pull-left" src="img/people/img2-small.jpg" alt="">
+                        <div class="pull-left">
+                            <span class="testimonials-name">Jake Witson</span>
+                            <span class="testimonials-post">Commercial Director</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Carousel nav -->
+            <a class="left-btn" href="#myCarousel" data-slide="prev"></a>
+            <a class="right-btn" href="#myCarousel" data-slide="next"></a>
+        </div>
+    </div>
+    <!-- END TESTIMONIALS -->
+    <!-- TESTIMONIALS -->
+    <div class="col-md-6 testimonials-v1">
+
+        <!-- BEGIN BLOCKQUOTE BLOCK -->
+        <div class="row quote-v1 margin-bottom-30">
+            <div class="col-md-12">
+                <span>Agradecimientos de la III Jornada Pedagógica</span>
+            </div>
+        </div>
+        <!-- END BLOCKQUOTE BLOCK -->
+        <div id="myCarousel2" class="carousel slide">
+            <!-- Carousel items -->
+            <div class="carousel-inner">
+                <div class="active item">
+                    <blockquote><p>Denim you probably haven't heard of. Lorem ipsum dolor met consectetur adipisicing sit amet, consectetur adipisicing elit, of them jean shorts sed magna aliqua. Lorem ipsum dolor met.</p></blockquote>
+                    <div class="carousel-info">
+                        <img class="pull-left" src="img/people/img1-small.jpg" alt="">
+                        <div class="pull-left">
+                            <span class="testimonials-name">Lina Mars</span>
+                            <span class="testimonials-post">Commercial Director</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <blockquote><p>Raw denim you Mustache cliche tempor, williamsburg carles vegan helvetica probably haven't heard of them jean shorts austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica.</p></blockquote>
+                    <div class="carousel-info">
+                        <img class="pull-left" src="img/people/img5-small.jpg" alt="">
+                        <div class="pull-left">
+                            <span class="testimonials-name">Kate Ford</span>
+                            <span class="testimonials-post">Commercial Director</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <blockquote><p>Reprehenderit butcher stache cliche tempor, williamsburg carles vegan helvetica.retro keffiyeh dreamcatcher synth. Cosby sweater eu banh mi, qui irure terry richardson ex squid Aliquip placeat salvia cillum iphone.</p></blockquote>
+                    <div class="carousel-info">
+                        <img class="pull-left" src="img/people/img2-small.jpg" alt="">
+                        <div class="pull-left">
+                            <span class="testimonials-name">Jake Witson</span>
+                            <span class="testimonials-post">Commercial Director</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Carousel nav -->
+            <a class="left-btn" href="#myCarousel2" data-slide="prev"></a>
+            <a class="right-btn" href="#myCarousel2" data-slide="next"></a>
+        </div>
+    </div>
+    <!-- END TESTIMONIALS -->
+</div>
+            <!-- END TABS AND TESTIMONIALS -->
