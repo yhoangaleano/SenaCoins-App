5f99b2eb271879ed2acaab7fa03e94b1fc34b758
sergiolondon2410 <sergiolondon2410@gmail.com>
2015-05-26 12:06:31 -0500

    Commit sergio v4

--- application/views/producto/create.php
+++ application/views/producto/create.php
@@ -1,10 +1,50 @@
-<h1>Registro de Producto</h1>
+<div class="row">
+	<div class="col-md-6">
 
-<form>
-	<input type="text" name="txtNomProducto" placeholder="Nombre de Equipo"><br>
-	<input type="text" name="txtNomUsuario" placeholder="Nombre de Usuario"><br>
-	<input type="text" name="txtContra" placeholder="contraseña"><br>
-	<input type="text" name="txtPass" placeholder="contraseña"><br>
-	<input type="text" name="txtPass" placeholder="contraseña"><br>
-	<input type="submit" name="btnIngresar" value="Crear">
-</form>
+		<div class="panel panel-orange">
+			<div class="panel-heading">Crear producto</div>
+			<div class="panel-body">
+				<form method="post">
+					<div class="form-group">
+						<label for="">Equipo</label>
+						<input type="text" class="form-control" id="txtIdEquipo" placeholder="Ejm: Multimedia"  />
+					</div>
+					<div class="form-group">
+						<label for="">Nombre del producto</label>
+						<input type="text" class="form-control" id="txtNombreProducto" placeholder="Ejm: Botellatrón"  />
+					</div>
+					<div class="form-group">
+						<label for="">Descripción</label>
+						<textarea name="descripcion" id="txtdescripcion" class="form-control" rows="5"></textarea>
+					</div>
+					<div class="form-group">
+						<button type="button" class="btn btn-primary pull-right" id="btnGuardar">Guardar</button>
+					</div>
+					<div class="form-group">
+						<button type="button" class="btn btn-block btn-primary" id="btnModificar" style="display:none">Modificar</button>
+					</div>
+				</form>
+			</div>
+		</div>
+
+	</div>
+	<div class="col-md-6">
+		<div class="form-group">
+
+			<table id="tblEquipo" class="table table-hover table-bordered">
+				<thead>
+					<tr>
+						<th class="active">Nombre del equipo</th>
+						<th class="active">Nombre de usuario</th>
+						<th class="active">Contraseña</th>
+						<th class="active">Cantidad de monedas</th>
+					</tr>
+				</thead>
+				<tbody>
+
+				</tbody>
+			</table>
+
+		</div>
+	</div>
+</div>	
