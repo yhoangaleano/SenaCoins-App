a5ed009ca6398a3fd244f33cfaac5032fe8fcfad
sergiolondon2410 <sergiolondon2410@gmail.com>
2015-05-27 19:28:13 -0500

    Deploy Sergio 27/05 19:26

--- application/data/SP_ModificarProducto.sql
+++ application/data/SP_ModificarProducto.sql
@@ -2,6 +2,6 @@
 -- Routine DDL
 -- Note: comments before and after the routine body will not be stored by the server
 -- --------------------------------------------------------------------------------
-CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_ModificarProducto`(IN `d_NombreProducto` VARCHAR(45), IN `d_Descripcion` VARCHAR(100), IN `d_IdEquipo` INT(11))
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_ModificarProducto`(IN `d_NombreProducto` VARCHAR(45), IN `d_Descripcion` VARCHAR(100), IN `d_IdProducto` INT(11))
     NO SQL
-UPDATE producto SET nombre_producto = d_NombreProducto, descripcion = d_Descripcion WHERE equipo_idEquipo = d_idEquipo
+UPDATE producto SET nombre_producto = d_NombreProducto, descripcion = d_Descripcion WHERE idProducto = d_idProducto
